51a85a8d088088564d0d7e57ca71a1628e53c07b
Matija Fućek <matijafucek1@gmail.com>
2025-05-01 21:36:51 +0200

    add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import { GeistSans } from 'geist/font/sans';
 import { type Metadata } from 'next';
 import { Toaster } from 'sonner';
 
+import { isProduction } from '@/constants';
 import { AnalyticsProvider } from '@/deps/posthog';
 import { cn } from '@/deps/shadcn/utils';
 import { TRPCReactProvider } from '@/deps/trpc/react';
 import { environmentPrefix } from '@/modules/metadata/constants';
 import { ThemeProvider } from '@/modules/theme/providers/theme-provider';
 import { ViewportSizeProvider } from '@/utils/use-viewport';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
 	title: environmentPrefix() + 'Crni Mag - likovne radionice',
@@ -45,6 +47,13 @@ export default function RootLayout({
 								{children}
 								<Toaster />
 							</body>
+							{isProduction && (
+								<Script
+									async
+									src="https://umami.crni-mag.studio/script.js"
+									data-website-id="bd299dcd-4d96-4247-a562-a597ecc85f9d"
+								/>
+							)}
 						</AnalyticsProvider>
 					</TRPCReactProvider>
 				</ViewportSizeProvider>
